8a0d60df8cdd5f3074f53e3794e1a8460655e384
Nathan attoh <nnaattoh@st.ug.edu.gh>
2025-09-26 14:31:25 +0000

    updated broker IP

--- read.py
+++ read.py
@@ -14,6 +14,6 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("197.255.72.236", 1883, 60)
+client.connect("197.255.72.230", 1883, 60)
 
 client.loop_forever()
